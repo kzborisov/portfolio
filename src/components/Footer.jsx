5157670952c3cddcd446227a5f12cd8a38f713c1
Kristian Borisov <kborisov@lely.com>
2023-08-31 11:22:29 +0300

    Fixed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,11 @@ const Footer = () => {
   return (
     <div className='text-primary md:mx-10 p-10 flex flex-col gap-4 items-center'>
       <div className='w-full h-[2px] bg-primary'></div>
-      <p className='text-xl'>&#169; Kristian Borisov - 2023 | All Right Reserved</p>
+      <p className='text-xl flex items-center justify-center flex-col'>
+        <div>&#169; Kristian Borisov - 2023</div>
+        <hr className='bg-primary w-full h-[1px] my-1' />
+        <div> All Right Reserved</div>
+      </p>
     </div>
   );
 };
